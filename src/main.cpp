702d48bed6c33389911eb5575979fbd810ca20d8
Przemyk <przemek@przemyk.xyz>
2021-03-28 11:02:24 +0200

    blending

--- src/main.cpp
+++ src/main.cpp
@@ -79,6 +79,8 @@ int main(int argc, char* argv[]) {
     debug::initOpenGL();
 
     glClearColor(0.15f, 0.15f, 0.15f, 1.0f);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
     shaderProgram = shaders::createProgram();
     
